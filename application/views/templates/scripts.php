8dd3680b6eedad9a7c30d00775f8be2fcc9000fb
Cristian Crulcich <cristiancrulcich@gmail.com>
2017-06-25 17:39:14 -0300

    Pantallas del refactor
    TPFINAL-109

---
+++ application/views/templates/scripts.php
@@ -0,0 +1,19 @@
+<!-- Javascript Libraries -->
+<script src="<?php echo base_url('vendors/bower_components/jquery/dist/jquery.min.js')?>"></script>
+<script src="<?php echo base_url('vendors/bower_components/bootstrap/dist/js/bootstrap.min.js')?>"></script>
+        
+<script src="<?php echo base_url('vendors/bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js')?>"></script>
+<script src="<?php echo base_url('vendors/bower_components/Waves/dist/waves.min.js')?>"></script>
+<script src="<?php echo base_url('vendors/bootstrap-growl/bootstrap-growl.min.js')?>"></script>
+<script src="<?php echo base_url('vendors/bootgrid/jquery.bootgrid.updated.min.js')?>"></script>
+<script src="<?php echo base_url('vendors/bower_components/bootstrap-sweetalert/lib/sweet-alert.min.js?')?>"></script>
+<script src="<?php echo base_url('vendors/bower_components/autosize/dist/autosize.min.js')?>"></script>
+
+<!-- Placeholder for IE9 -->
+<!--[if IE 9 ]>
+	<script src="<?php echo base_url('vendors/bower_components/jquery-placeholder/jquery.placeholder.min.js')?>"></script>
+<![endif]-->
+        
+<script src="<?php echo base_url('js/functions.js')?>"></script>
+<script src="<?php echo base_url('js/actions.js')?>"></script>
+<script src="<?php echo base_url('js/demo.js')?>"></script>
