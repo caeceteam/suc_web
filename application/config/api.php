e60ea806a7c41c648ea28288713bfc866620461c
kevineuman <kevineuman@gmail.com>
2017-08-30 19:52:43 -0300

    Configuración para Heroku

--- application/config/api.php
+++ application/config/api.php
@@ -1,4 +1,4 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed.');
 
-$config['api_base_uri']     = 'http://localhost:3000';           	
+$config['api_base_uri']     = 'https://sucapicaece.herokuapp.com/';           	
 $config['api_timeout']      = 5.0;
