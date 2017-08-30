1f898ca0a61e6d482bfba4074913e87979069f5b
Cristian Crulcich <cristiancrulcich@gmail.com>
2017-08-30 19:35:41 -0300

    TPFINAL-121: Agregados files de concurrente y js de la api de google

---
+++ application/models/Assistant_model.php
@@ -0,0 +1,167 @@
+<?php
+
+use GuzzleHttp\Client;
+use function GuzzleHttp\json_decode;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ServerException;
+
+class Assistant_model extends CI_Model {
+	/**
+	 * Variables para el rest client
+	 * @var string
+	 */
+	private $base_uri 	= 'http://localhost:3000';
+	private $client;
+	private $timeout = 5.0;
+	
+	/**
+	 * Variables para los atributos del modelo
+	 * @var string
+	 */
+	public $id;
+	public $idDiner;
+	public $name;
+	public $surname;
+	public $bornDate;
+	public $street;
+	public $streetNumber;
+	public $floor;
+	public $door;
+	public $zipcode;
+	public $phone;
+	public $contactName;
+	public $scholarship;
+	public $eatAtOwnHouse;
+	public $economicSituation;
+	public $celiac;
+	public $diabetic;
+	public $document;
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->client = new Client([
+			'base_uri' => $this->base_uri,
+			'timeout'  => $this->timeout,
+			]);
+	}
+	
+	/**
+	 * Consulta asistentes
+	 * 
+	 * Consulta de asistentes a la API
+	 * @param 		string 		$url
+	 * @return 		array 		Si la consulta fue exitosa devuelve un array, sino devuelve NULL
+	 */
+	private function search($url)
+	{
+		$response = $this->client->request('GET', $url);		
+		if($response->getStatusCode()==HTTP_OK)
+		{
+			$body = $response->getBody();
+			return json_decode($body,TRUE);
+		}
+		else
+			return NULL;		
+	}
+	
+	/**
+	 * Consulta de concurrentes by id
+	 * @param 	int 	$id
+	 */
+	public function search_by_id($id)
+	{
+		$url = 'api/assistants/' . $id;
+		return $this->search($url);
+	}
+	
+	/**
+	 * Consulta de concurrentes por página para el listado
+	 * @param 	string 	$page
+	 */
+	public function get_assistants_by_page($page)
+	{
+		$url = 'api/assistants?page=' . $page;
+		return $this->search($url);
+	}
+		
+	/**
+	 * Alta de concurrentes
+	 * @param		object	$assistant
+	 * @return 		array   Si el alta fue exitosa, devuelve un array con el assistant, sino devuelve NULL
+	 */		
+	public function add($assistant)
+	{
+		try {
+			$response = $this->client->request('POST', 'api/assistants', [
+				'json' => $assistant
+			]);
+			if($response->getStatusCode()==HTTP_CREATED)
+			{
+				$body = $response->getBody();
+				return json_decode($body,TRUE);
+			}
+			return NULL;
+		}
+		catch (ServerException $e) {
+			return $this->errorMessage($e);
+		}
+	}
+	
+	/**
+	 * Edición de concurrente
+	 * @param		object	$assistant
+	 * @return 		array   Si la edición fue exitosa, devuelve un array con el assistant, sino devuelve NULL
+	 */
+	public function edit($assistant)
+	{
+		try {
+			$response = $this->client->request('PUT', 'api/assistants/' . $assistant->id, [
+					'json' => $assistant
+			]);
+			if($response->getStatusCode()==HTTP_ACCEPTED)
+			{
+				$body = $response->getBody();
+				return json_decode($body,TRUE);
+			}
+			else
+				return NULL;
+		}
+		catch (Exception $e) {
+			return $this->errorMessage($e);
+		}
+	}
+	
+	/**
+	 * Función que mapea el mensaje de error desde la API usado en los editores
+	 * @param 	exception $exceptionData
+	 */
+	private function errorMessage($exceptionData) 
+	{
+		$errorResponse = json_decode($exceptionData->getResponse()->getBody(), TRUE);
+		$errorResponse['errors'] = TRUE;
+		if($exceptionData->getCode() == HTTP_INTERNAL_SERVER)
+		{
+			return $errorResponse;
+		}
+		return NULL;
+	}
+	
+	/**
+	 * Delete de concurrentes
+	 * @param		string	$id
+	 * @return 		bool   Si la baja fue exitosa, devuelve un array con el assistant, sino devuelve NULL
+	 */
+	public function delete($id)
+	{
+		$response = $this->client->request('DELETE', 'api/assistants/' . $id);
+		if($response->getStatusCode()==HTTP_OK)
+		{
+			return TRUE;
+		}
+		else
+			return FALSE;
+	}
+};
+
---
+++ application/views/assistant/search.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--[if IE 9 ]><html class="ie9"><![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SUC</title>
+        
+		<?php $this->load->view('templates/styles'); ?>
+    </head>
+
+    <body data-ma-header="teal">
+
+		<?php $this->load->view('templates/header'); ?>
+
+        <section id="main">
+			
+			<?php $this->load->view('templates/menu'); ?>
+			
+            <section id="content">
+                <div class="container">
+                    <div class="c-header">
+                        <h2>Concurrentes</h2>
+                    </div>
+
+					<div class="card">
+						<div class="card-body card-padding">
+							<form class="row" role="form">
+                                <div class="col-sm-4">
+                                    <div style="position: relative;display: block;margin-top: 10px;margin-bottom: 10px;"> <!--TODO CC: Pass style inline to css class-->
+                                        <label>
+                                            ¿Desea crear un nuevo concurrente?
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <a href="<?php echo base_url('assistant/add');?>" class="btn btn-primary btn-sm m-t-5 waves-effect">Crear</a>
+                                </div>
+                            </form>			
+						</div>
+					</div>					
+					
+					<div class="card">
+						<div class="card-body card-padding" style="padding-bottom:0"></div> <!--TODO CC: Pass style inline to css class-->
+						<table id="data-table-command" class="table table-striped table-vmiddle bootgrid-table">
+						    <thead>
+						        <tr>
+						            <th data-column-id="idAssistant" data-visible="false">ID</th>
+						            <th data-column-id="name" data-order="desc">Nombre</th>
+						            <th data-column-id="surname" data-order="desc">Apellido</th>
+									<th data-column-id="address">Domicilio</th>
+									<th data-column-id="phone">Teléfono</th>
+									<th data-column-id="commands" data-formatter="commands" data-sortable="false">Modificar/Borrar</th>
+						        </tr>
+						    </thead>
+						</table>
+                    </div>
+                </div>
+                <input hidden id="data-request-url" value="<?php echo isset($_ci_vars['data-request-url']) ? $_ci_vars['data-request-url'] : '' ?>"></input>
+                
+            </section>
+
+			<?php $this->load->view('templates/footer'); ?>
+			
+        </section>
+
+		<?php $this->load->view('templates/scripts'); ?>
+		<script src="<?php echo base_url('js/tableGrid.js')?>"></script>
+		
+		<!-- Data Table -->
+        <script type="text/javascript">
+        	loadBootgrid({
+        		selector: "#data-table-command",
+        		requestUrl: $("#data-request-url")[0].value,
+        		noResultText: "No hay concurrentes cargados",
+        		infos: "Viendo {{ctx.start}} de {{ctx.end}} de {{ctx.total}} concurrentes",
+        		editUrl: "<?php echo site_url('assistant/edit/') ?>",
+        		deleteUrl: "<?php echo site_url('assistant/delete/') ?>",
+        		deleteDialogTitle: "¿Está seguro en borrar al concurrente seleccionado?",
+        		deleteDialogText: "El concurrente se borrará permanentemente del sistema",
+        		deleteDialogSuccess: "El concurrente se ha borrado del sistema."
+            });
+
+        </script>
+
+	</body>
+</html>
