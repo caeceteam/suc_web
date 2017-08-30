0eebdb7f8446024c1090b7c18ee21811239001b3
kevineuman <kevineuman@gmail.com>
2017-08-30 20:02:35 -0300

    Cambio de https a http de BASE_URL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://sucapicaece.herokuapp.com/suc_web';
+$config['base_url'] = 'http://sucapicaece.herokuapp.com/suc_web';
 
 /*
 |--------------------------------------------------------------------------
