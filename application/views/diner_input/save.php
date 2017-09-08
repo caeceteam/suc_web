2c5ef4d6b66a18ad635df46f2529c1fc9329ab8f
Marco Cupo <cuttlas88@yahoo.com.ar>
2017-09-08 18:07:50 -0300

    Se deshabilitan campos en el modo edición TPFINAL-111

--- application/views/diner_input/save.php
+++ application/views/diner_input/save.php
@@ -49,7 +49,7 @@
 											<div class="col-sm-4">
 												<div class="form-group fg-float">
 													<div class="fg-line" data-id="code">
-														<input type="text" id="name" name="name" class="input-sm form-control fg-input" value="<?php echo ($reset) ? '' : set_value('name',$this->form_data->name); ?>">
+														<input type="text" id="name" name="name" class="input-sm form-control fg-input" <?php echo ($_ci_vars['request-action'] == "PUT") ? 'disabled=""' : ''; ?> value="<?php echo ($reset) ? '' : set_value('name',$this->form_data->name); ?>">
 														<label class="fg-label">Nombre</label>
 													</div>
 												</div>
@@ -58,7 +58,7 @@
 											<div class="col-sm-4">
 												<div class="form-group fg-float">
 													<div class="fg-line" data-id="code">
-														<input type="text" id="size" name="size" class="input-sm form-control fg-input" value="<?php echo ($reset) ? '' : set_value('size',$this->form_data->size); ?>">
+														<input type="text" id="size" name="size" class="input-sm form-control fg-input" <?php echo ($_ci_vars['request-action'] == "PUT") ? 'disabled=""' : ''; ?> value="<?php echo ($reset) ? '' : set_value('size',$this->form_data->size); ?>">
 														<label class="fg-label">Talle</label>
 													</div>
 												</div>
@@ -69,7 +69,7 @@
 											<div class="col-sm-4">
 												<div class="form-group fg-float">
 													<div class="fg-line" data-id="code">
-														<input type="text" id="genderType" name="genderType" class="input-sm form-control fg-input" value="<?php echo ($reset) ? '' : set_value('genderType',$this->form_data->genderType); ?>">
+														<input type="text" id="genderType" name="genderType" class="input-sm form-control fg-input" <?php echo ($_ci_vars['request-action'] == "PUT") ? 'disabled=""' : ''; ?> value="<?php echo ($reset) ? '' : set_value('genderType',$this->form_data->genderType); ?>">
 														<label class="fg-label">Género</label>
 													</div>
 												</div>
