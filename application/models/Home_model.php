bae88ba3b3758fa2381208f329e43834fc20b949
kevineuman <kevineuman@gmail.com>
2017-09-22 19:17:47 -0300

    Agrego los datos de sesión: 'headers'  => ['x-access-token' => $this->session->token],

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -24,7 +24,7 @@ class Home_model extends CI_Model {
 		$this->base_uri = $this->config->item('api_base_uri');
 		$this->timeout	= $this->config->item('api_timeout');
 		$this->client = new Client([
-			'headers'  => ['x-access-token' => $this->session->token], //header con datos de sesión
+			'headers'  => ['x-access-token' => $this->session->token], //header con datos de sesión 
 			'base_uri' => $this->base_uri,
 			'timeout'  => $this->timeout,
 			]);
