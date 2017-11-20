997b474b88d25e0c5ec4fbd0923e20cd4929cff4
kevineuman <kevineuman@gmail.com>
2017-11-20 20:49:06 -0300

    check en Admin Application fix confirmDialog

--- application/controllers/Admin_application.php
+++ application/controllers/Admin_application.php
@@ -41,7 +41,7 @@ class Admin_application extends CI_Controller {
 	public function index()
 	{
 		$this->variables['data-request-url'] = site_url('admin_application/render_table_response');
-		$this->load->view('admin_application/search', $this->variables);
+		$this->load->view($this->strategy_context->get_url('admin_application/search'), $this->variables);
 	}
 
 	/**
@@ -146,20 +146,22 @@ class Admin_application extends CI_Controller {
 		$this->variables['reject-url'] = site_url('admin_application/reject');
 		
 		$diner = $this->Diner_application_model->search_by_id($id);
-		$this->form_data->id 				= $diner['idDiner'];
-		$this->form_data->photo				= isset($diner['photos'][0]) ? $diner['photos'][0]['url'] : '';
-		$this->form_data->id_user			= $diner['user']['idUser'];
-		$this->form_data->alias				= $diner['user']['alias'];
-		$this->form_data->user_name			= $diner['user']['name'];
-		$this->form_data->surname			= $diner['user']['surname'];
-		$this->form_data->user_mail			= $diner['user']['mail'];
-		$this->form_data->diner_name		= $diner['name'];
-		$this->form_data->street			= $diner['street'];
-		$this->form_data->streetNumber		= $diner['streetNumber'];
-		$this->form_data->floor				= $diner['floor'];
-		$this->form_data->door				= $diner['door'];
-		$this->form_data->diner_phone		= $diner['phone'];
-			
+		if ( $diner == !NULL )
+		{
+			$this->form_data->id 				= $diner['idDiner'];
+			$this->form_data->photo				= isset($diner['photos'][0]) ? $diner['photos'][0]['url'] : '';
+			$this->form_data->id_user			= $diner['user']['idUser'];
+			$this->form_data->alias				= $diner['user']['alias'];
+			$this->form_data->user_name			= $diner['user']['name'];
+			$this->form_data->surname			= $diner['user']['surname'];
+			$this->form_data->user_mail			= $diner['user']['mail'];
+			$this->form_data->diner_name		= $diner['name'];
+			$this->form_data->street			= $diner['street'];
+			$this->form_data->streetNumber		= $diner['streetNumber'];
+			$this->form_data->floor				= $diner['floor'];
+			$this->form_data->door				= $diner['door'];
+			$this->form_data->diner_phone		= $diner['phone'];
+		}	
 		$this->load->view($this->strategy_context->get_url('admin_application/save'), $this->variables);
 	}
 		
