00c421d55b662e727515cc63d84ae7794dc20bac
Marco Cupo <mcupo@users.noreply.github.com>
2017-11-20 11:45:01 -0300

    Se habia vuelto a romper la ruta de los errores

--- application/config/config.php
+++ application/config/config.php
@@ -282,7 +282,7 @@ $config['log_date_format'] = 'Y-m-d H:i:s';
 | application/views/errors/ directory.  Use a full server path with trailing slash.
 |
 */
-$config['error_views_path'] = '/application/views/errors/html/';
+$config['error_views_path'] = '/application/views/errors/';
 
 /*
 |--------------------------------------------------------------------------
