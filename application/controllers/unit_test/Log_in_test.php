1d9f96d1192131766060f75c5930efd4d989daed
lasuperfoca <carb.jose@gmail.com>
2017-11-20 19:00:02 -0300

    Unit Test - Log In

--- application/controllers/unit_test/Log_in_test.php
+++ application/controllers/unit_test/Log_in_test.php
@@ -8,13 +8,18 @@ class Log_in_test extends CI_Controller {
     
     public $id_test;
     
-    //Test Creación
+    //Test User Log In
     public $user_name_test          = 'tony' ;
+    public $user_pass_test          = '6d4718a9';
+    
+    //Test Mail Log In
     public $user_mail_test          = 'carb.jose@gmail.com';
-    public $user_pass_test          = '123456' ;
-    public $user_pass_error_test    = 'RRRRRR' ;
-    public $user_new_pass           = '654321';
-    public $user_new_rep_pass       = '654321';
+    public $user_mail_pass_test     = '6d4718a9';
+    
+    //Test Log in erroneo
+    public $user_pass_error_test    = 'RRRRRR'  ;
+    
+    
     
     /**
      * Unit Test - Constructor Food Type
@@ -23,7 +28,7 @@ class Log_in_test extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->library(array( 'login'));
+        $this->load->library(array( 'login', 'session'));
         $this->load->model('Login_model');
         $this->load->library('unit_test');
     }
@@ -34,18 +39,21 @@ class Log_in_test extends CI_Controller {
      */
     public function index()
     {
-         
-    
         //Test - Altas
         $this->log_in_test('T001 - Login Correcto -  Usuario y clave correcta');
-        $this->log_out_test('T002 - Logout Correcto -  Usuario y clave correcta');
+        $this->log_in_mail_test('T002 - Login Correcto -  Correo del Usuario y clave correcta');
         
+        //Test Log Out
+        $this->log_out_test('T003 - Logout Correcto -  Usuario y clave correcta');
+        $this->log_in_err_test('T004 - Login Incorrecto -  Usuario y clave incorrecta');
+        
+        //Test 
+        $this->log_in_forgot_test('T005 - Login Correcto - Olvido de contraseña');
         echo $this->unit->report();
     }
-    
-    
+        
     /**
-     * Alta nuevo tipo - Caso satisfactorio
+     * Log In - Caso satisfactorio
      * @return void
      */
     public function log_in_test($tit_test)
@@ -55,10 +63,82 @@ class Log_in_test extends CI_Controller {
         $user->password	= $this->user_pass_test;
         
         $test             = $this->Login_model->validate($user);
-        $this->id_test    = $test['idFoodType'];
+        $test_name        = $tit_test;
+        $notes            = var_export($test, true);
+        $this->unit->run($test, 'is_array' , $test_name, $notes);
+    }
+       
+    /**
+     * Log Out - Caso satisfactorio
+     * @return void
+     */
+    public function log_out_test($tit_test)
+    {   
+        $test = true;
+        try{
+           $this->session->sess_destroy();
+        }catch (Exception $e){ $test = false; }
+        
+        $test_name        = $tit_test;
+		$notes            = var_export($test, true);
+		$this->unit->run($test, 'is_true' , $test_name, $notes);
+    }
+
+    /**
+     * Log In - Caso satisfactorio con mail
+     * @return void
+     */
+    public function log_in_mail_test($tit_test)
+    {   
+        //Crear datos
+        $user 			  = new stdClass();
+        $user->userName   = $this->user_mail_test;
+        $user->password	  = $this->user_mail_pass_test;
+        
+        //Inicio del test
+        $test             = $this->Login_model->validate($user);
         $test_name        = $tit_test;
         $notes            = var_export($test, true);
         $this->unit->run($test, 'is_array' , $test_name, $notes);
     }
 
+    /**
+     * Log In - Caso satisfactorio con mail
+     * @return void
+     */
+    public function log_in_err_test($tit_test)
+    {
+        //Crear datos
+        $user 			  = new stdClass();
+        $user->userName   = $this->user_name_test;
+        $user->password	  = $this->user_pass_error_test;
+    
+        //Inicio del test
+        $test             = $this->Login_model->validate($user);
+        $test_name        = $tit_test;
+        $notes            = var_export($test, true);
+        $this->unit->run($test, 'is_null' , $test_name, $notes);
+    }
+
+    
+    /**
+     * Log In lost usuario - Caso satisfactorio con mail
+     * @return void
+     */
+    public function log_in_forgot_test($tit_test)
+    {
+        //Crear datos
+		$password 				= new stdClass();
+		$password->userName 	= $this->user_name_test;
+		$password->newPassword	= $this->user_pass_test;
+    
+        //Inicio del test
+        $test             = $this->Login_model->reset_password($password);
+        $test_name        = $tit_test;
+        $notes            = var_export($test, true);
+        $this->unit->run($test, 'is_array' , $test_name, $notes);
+         
+    }
+    
+    
 }
