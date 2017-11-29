fdc7fb37b42b3beddd8f4b88522ecdad010928d6
lasuperfoca <carb.jose@gmail.com>
2017-11-29 18:57:41 -0300

    Correccion Roles

--- application/views/templates/userDinerRol.php
+++ application/views/templates/userDinerRol.php
@@ -3,9 +3,9 @@
 		data-placeholder="<?php echo ($reset) ? 'Tareas en el comedor' : set_value('rol', utf8_decode($this->form_data->role)); ?>"
 		id="role" name="role" >
 		<option value="99"></option>
-		<option value=DINER_ADMIN
+		<option value= <?php echo(DINER_ADMIN)?>
 			<?php if ($this->form_data->role == DINER_ADMIN || $this->form_data->role == '' ){echo 'selected';} ?>>Administrador</option>
-		<option value=EMPLOYEE
+		<option value=<?php echo(EMPLOYEE) ?>
 			<?php if ($this->form_data->role == EMPLOYEE){echo 'selected';}; ?>>Colaborador
 			del comedor</option>
 	</select>
--- application/views/templates/userDinerRolSysAdm.php
+++ application/views/templates/userDinerRolSysAdm.php
@@ -2,9 +2,9 @@
 	<label>Rol</label> <select class="chosen"
 		data-placeholder="<?php echo ($reset) ? 'Tareas en el comedor' : set_value('rol', utf8_decode($this->form_data->role)); ?>"
 		id="role" name="role" >
-		<option value=SYS_ADMIN
+		<option value= <?php echo(SYS_ADMIN)?>
 			<?php if ($this->form_data->role == SYS_ADMIN || $this->form_data->role == '' ){echo 'selected';} ?>>Administrador de Sistema</option>
-		<option value=GUEST
+		<option value= <?php echo(GUEST) ?>
 			<?php if ($this->form_data->role == GUEST){echo 'selected';}; ?>>Invitado</option>
 	</select>
 </div>
